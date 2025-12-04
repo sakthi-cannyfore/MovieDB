80ca00d365a072a234248a4c4d18adaf08ec7dc4
sakthimurugesanr <sakthicareer001@gmail.com>
2025-12-04 12:46:30 +0530

    creted the app

--- src/Components/People/People.tsx
+++ src/Components/People/People.tsx
@@ -17,8 +17,8 @@ const People = () => {
     }
   }, [movieId, dispatch]);
 
-  if(person?.length == 0 ){
-    <div>There is no people 's Found</div>
+  if (person?.length == 0) {
+    <div>There is no people 's Found</div>;
   }
 
   return (
@@ -26,34 +26,39 @@ const People = () => {
       <h1 className="font-bold text-1xl m-3">People</h1>
 
       {person?.length === 0 ? (
-  <div className="flex justify-center text-gray-400">There is no people found</div>
-) : (
-  person?.map((p: any) => (
-    <div key={p.id} className="flex items-center">
-      <div>
-        <img
-          src={p.profile_path ? `https://image.tmdb.org/t/p/w200${p.profile_path}` : noman}
-          className="w-[80px] h-[80px] object-cover m-3 rounded-md"
-          alt={p.name}
-        />
-      </div>
-      <div>
-        <h1 className="font-bold text-lg">{p.name}</h1>
-        <div className="flex items-center gap-2 flex-wrap">
-          <h1>Acting •</h1>
-          {p.known_for.map((item: any,index:any) => (
-            <Link to={`/movie/${item.id}`} key={`${item.id}-${index}`}>
-              <h1 className="text-blue-600 cursor-pointer">
-                {item.name || item.title}
-              </h1>
-            </Link>
-          ))}
+        <div className="flex justify-center text-gray-400">
+          There is no people found
         </div>
-      </div>
-    </div>
-  ))
-)}
-
+      ) : (
+        person?.map((p: any, ) => (
+          <div key={p.id} className="flex items-center">
+            <div>
+                <img
+                  src={
+                    p.profile_path
+                      ? `https://image.tmdb.org/t/p/w200${p.profile_path}`
+                      : noman
+                  }
+                  className="w-[80px] h-[80px] object-cover m-3 rounded-md"
+                  alt={p.name}
+                />
+            </div>
+            <div>
+              <h1 className="font-bold text-lg">{p.name}</h1>
+              <div className="flex items-center gap-2 flex-wrap">
+                <h1>Acting •</h1>
+                {p.known_for.map((item: any, index: any) => (
+                  <Link to={`/movie/${item.id}`} key={`${item.id}-${index}`}>
+                    <h1 className="text-blue-600 cursor-pointer">
+                      {item.name || item.title}
+                    </h1>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
